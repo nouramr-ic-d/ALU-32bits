ac1f97d73f8439b3b13994cbe9e9fff553cdb6c0
‪Nour Amr‬‏ <namr41004@gmail.com>
2023-08-04 19:52:58 +0300

    Create Alu.v

---
+++ Alu.v
@@ -0,0 +1,33 @@
+
+
+
+module ALU(
+    
+        input [15:0] A,
+        input [15:0] B,
+        input [2:0] op ,
+        output reg[31:0]Alu_out,
+  
+        
+      
+    );
+    always@(*)
+    begin
+             case(op)
+             3'b000 : Alu_out=A+B;
+             3'b001 : Alu_out=A-B;
+             3'b010 : Alu_out=A*B;
+             3'b011 : Alu_out=A/B;
+             3'b011 : Alu_out=A%B; 
+             3'b100 : Alu_out=A|B;
+             3'b101 : Alu_out=A&B;
+             3'b110 : Alu_out=~A;
+             3'b111 : Alu_out=~B;
+             default: Alu_out=0;
+             endcase
+        
+    
+ end
+
+
+endmodule
