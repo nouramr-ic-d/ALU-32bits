8ff5add12f2df6fbcd3322f814179a2f4ac3c499
‪Nour Amr‬‏ <namr41004@gmail.com>
2023-08-05 03:06:08 +0300

    Update Alu.v

--- Alu.v
+++ Alu.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/29/2023 03:27:32 AM
-// Design Name: 
-// Module Name: ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ALU(
     
